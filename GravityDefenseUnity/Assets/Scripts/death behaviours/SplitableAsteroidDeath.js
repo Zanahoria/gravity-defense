99602cd1014b657f8531bc32c6486a384c8d574c
Bertin <matthieu.bertin@epitech.eu>
2013-05-15 16:57:04 +0800

    restore some modifications

--- GravityDefenseUnity/Assets/Scripts/death behaviours/SplitableAsteroidDeath.js
+++ GravityDefenseUnity/Assets/Scripts/death behaviours/SplitableAsteroidDeath.js
@@ -20,14 +20,10 @@ function Update () {
 		
 		if (this.GetComponent(Life).maxLife > 1 )
 		{
-<<<<<<< HEAD
-			var newAsteroid1 = Instantiate(this, this.transform.position + Vector3(0.5, 0, 0), Quaternion.identity);
-=======
 			var decalVec : Vector3 = calcDecalVec();
 			var newAsteroid1 = Instantiate(this, this.transform.position + decalVec * (this.transform.localScale.x / 2.0 + 0.1), Quaternion.identity);
 			newAsteroid1.transform.localScale = this.transform.localScale / 2.0;
 			newAsteroid1.rigidbody.velocity = this.rigidbody.velocity + decalVec * 0.5;
->>>>>>> dedfa6cd4744803cd1c2be1b90f21c17dbe09cec
 			newAsteroid1.GetComponent(Life).maxLife = this.GetComponent(Life).maxLife / 2;
 			newAsteroid1.GetComponent(Life).currentLife = newAsteroid1.GetComponent(Life).maxLife;
 			newAsteroid1.GetComponent(AsteroidSettings).nbResourcesEarned = this.GetComponent(AsteroidSettings).nbResourcesEarned;
@@ -36,13 +32,9 @@ function Update () {
 			++LevelDescriptor.nbEnemies;
 			LevelDescriptor.asteroidTab.Add(newAsteroid1.gameObject);
 			
-<<<<<<< HEAD
-			var newAsteroid2 = Instantiate(this, this.transform.position - Vector3(0.5, 0, 0), Quaternion.identity);
-=======
 			var newAsteroid2 = Instantiate(this, this.transform.position - decalVec * (this.transform.localScale.x / 2.0 + 0.1), Quaternion.identity);
 			newAsteroid2.transform.localScale = this.transform.localScale / 2.0;
 			newAsteroid2.rigidbody.velocity = this.rigidbody.velocity - decalVec * 0.5;
->>>>>>> dedfa6cd4744803cd1c2be1b90f21c17dbe09cec
 			newAsteroid2.GetComponent(Life).maxLife = this.GetComponent(Life).maxLife / 2;
 			newAsteroid2.GetComponent(Life).currentLife = newAsteroid2.GetComponent(Life).maxLife;
 			newAsteroid2.GetComponent(AsteroidSettings).nbResourcesEarned = this.GetComponent(AsteroidSettings).nbResourcesEarned;
@@ -59,7 +51,7 @@ function Update () {
 					asteroidTab.RemoveAt(i);
 					break;
 				}
-			}	
+			}
 		Destroy(this.gameObject);
 			
 		
