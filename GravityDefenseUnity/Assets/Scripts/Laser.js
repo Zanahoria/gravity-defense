26ae6ea3b04993d24706ac33d713ffea52c3e1fa
petitf-q <quentin.petitfils@epitech.eu>
2013-05-15 22:31:01 +0800

    laser modif

--- GravityDefenseUnity/Assets/Scripts/Laser.js
+++ GravityDefenseUnity/Assets/Scripts/Laser.js
@@ -1,9 +1,9 @@
 #pragma strict
 
-public var activated : int = 0;
-public static var LASER_RANGE : int = 5;
-public static var RATE_OF_FIRE : float = 0.6; //time between shot in seconds
-public static var DAMAGE : int = 1;
+public var activated : boolean = false;
+public var LASER_RANGE : int = 5;
+public var RATE_OF_FIRE : float = 0.6; //time between shot in seconds
+public var DAMAGE : int = 2z;
 private var lastHit : GameObject = null;
 private var TimeSinceLast : float = RATE_OF_FIRE;
 
@@ -15,14 +15,7 @@ function Update () {
 	if (activated && ((Time.timeSinceLevelLoad - TimeSinceLast) >= RATE_OF_FIRE) && LevelDescriptor.state == LevelDescriptor.ISROUNDING)
 	{
 		var LaserPos = gameObject.transform.position;
-		if (lastHit)
-		{
-				if ((Vector3.Distance(LaserPos, lastHit.gameObject.transform.position)) < LASER_RANGE)
-				{
-					lastHit.GetComponent(Life).currentLife -= 1;
-    			}
-		}
-		else
+		if (!lastHit || ((Vector3.Distance(LaserPos, lastHit.gameObject.transform.position)) > LASER_RANGE))
 		{
 			var asteroids = LevelDescriptor.asteroidTab;
     		for (var i : GameObject in asteroids)
@@ -31,11 +24,15 @@ function Update () {
 				if (dist < LASER_RANGE)
 				{
 					lastHit = i;
-					lastHit.GetComponent(Life).currentLife -= DAMAGE;
+					Debug.Log("nouveau last hit");
 					break;
     			}
     		}
     	}
-    	TimeSinceLast = Time.timeSinceLevelLoad;
+    	if (lastHit)
+    	{
+	   	lastHit.GetComponent(Life).currentLife -= DAMAGE;
+	   	TimeSinceLast = Time.timeSinceLevelLoad;
+		}
 	}
 }
