61d3c2dc7648326820c20cb5de3c297cd3e28c1d
castel_a <gianni.castel@gmail.com>
2013-05-16 03:09:57 +0800

    change asteroid range destruction algorithm

--- GravityDefenseUnity/Assets/Scripts/AsteroidSettings.js
+++ GravityDefenseUnity/Assets/Scripts/AsteroidSettings.js
@@ -3,26 +3,31 @@
 public var initialVelocity : float = 0;
 public var nbResourcesEarned : int = 5;
 
-private var duration : float = 5;
-private var spawnTime : float = 0;
+private var isComing;
 
 function Awake()
 {
 	var pos = this.transform.position;
 	rigidbody.velocity = transform.TransformDirection((-pos * initialVelocity) / pos.magnitude);
 	rigidbody.angularVelocity = Vector3(Random.Range(-4, 4), Random.Range(-4, 4), Random.Range(-4, 4));
-
-	spawnTime = Time.timeSinceLevelLoad;
 }
 
 function Start () {
-
+	isComing = true;
 }
 
 function Update () {
-	if (transform.position.magnitude > 35 && Time.timeSinceLevelLoad > spawnTime + duration)
+	if (transform.position.magnitude < 35)
 	{
-		--LevelDescriptor.nbEnemies;
-		LevelDescriptor.RemoveAsteroid(this.gameObject);
+		if (isComing)
+			isComing = false;
+	}
+	else
+	{
+		if (!isComing)
+		{
+			--LevelDescriptor.nbEnemies;
+			LevelDescriptor.RemoveAsteroid(this.gameObject);
+		}
 	}
 }
