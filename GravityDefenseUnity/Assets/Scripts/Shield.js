6bf0fabcd972e3d97321e47fa2b20dec80da6f4a
pierre nole <pierre.nole@gmail.com>
2013-05-16 15:33:01 +0800

    added shield visuals

--- GravityDefenseUnity/Assets/Scripts/Shield.js
+++ GravityDefenseUnity/Assets/Scripts/Shield.js
@@ -3,22 +3,38 @@
 public var maxShield : int = 0;
 public var currentShield : int = 0;
 
+public var mat_shield_bounce : Material;
+public var mat_shield_destruct : Material;
+
 function Start () {
 	maxShield = 0;
 	currentShield = 0;
 }
 
 function Update () {
+	var mat;
 	if (LevelDescriptor.state == LevelDescriptor.ISWAITING)
 		currentShield = maxShield;
+	if (currentShield > 0) {
+		if (gameObject.GetComponent(Upgrade).TypePlanet == Upgrade.DESTRUCTION)
+			mat = [ renderer.materials[0], mat_shield_destruct ];
+		else
+			mat = [ renderer.materials[0], mat_shield_bounce ];
+	}
+	else {
+		mat = [ renderer.materials[0] ];
+	}
+	renderer.materials = mat;
 }
 
 public function AddRemoveShield(shield : int)
 {
-	if (currentShield + shield > maxShield)
+	if (currentShield + shield > maxShield) {
 		currentShield = maxShield;
-	else if (currentShield + shield < 0)
+	}
+	else if (currentShield + shield < 0) {
 		currentShield = 0;
+	}
 	else
 		currentShield += shield;
 }
