b8d10786adb5006dd6301ad1aba481cafe4ea8a9
Bertin <matthieu.bertin@epitech.eu>
2013-05-16 15:24:35 +0800

    add tuto level

--- GravityDefenseUnity/Assets/Scripts/LevelScript.js
+++ GravityDefenseUnity/Assets/Scripts/LevelScript.js
@@ -1,55 +1,237 @@
 #pragma strict
-
 // level name and description
-LevelDescriptor.levelName = "Test";
-LevelDescriptor.levelDescription = "a basic test";
+LevelDescriptor.levelName = "Level 1";
+LevelDescriptor.levelDescription = "a basic level 1";
 
 // initial resources
-MineralResources.nbResources = 200;
+MineralResources.nbResources = 100;
 
-// first round
 var round : Round;
 var wave : Wave;
 var direction : Direction;
 
+// first round
 round = LevelDescriptor.CreateRound();
+round.name = "add your first planet";
 wave = LevelDescriptor.CreateWave();
+
 direction = LevelDescriptor.CreateDirection();
-direction.asteroidType = "ship";
-direction.asteroidNb = 16;
+direction.angle = convertAngle(45);
+direction.asteroidType = "basic";
+direction.asteroidNb = 5;
+direction.asteroidLife = 1;
+direction.moneyEarned = 5;
+
 wave.AddDirection(direction);
-
 round.AddWave(wave);
-
 LevelDescriptor.AddRound(round);
 
 // second round
 round = LevelDescriptor.CreateRound();
+round.name = "move your planet";
 wave = LevelDescriptor.CreateWave();
-direction = LevelDescriptor.CreateDirection();
-direction.angle = 90.0 * 3.14 / 180.0;
-direction.asteroidType = "splittable";
-direction.asteroidLife = 8;
-direction.asteroidScale = 8;
-direction.asteroidNb = 1;
 
+direction = LevelDescriptor.CreateDirection();
+direction.angle = convertAngle(120);
+direction.asteroidType = "basic";
+direction.asteroidNb = 5;
+direction.asteroidLife = 1;
+direction.moneyEarned = 5;
+
+wave.AddDirection(direction);
+round.AddWave(wave);
+LevelDescriptor.AddRound(round);
+
+// third round
+round = LevelDescriptor.CreateRound();
+round.name = "Colision asteroids between them";
+wave = LevelDescriptor.CreateWave();
+
+direction = LevelDescriptor.CreateDirection();
+direction.angle = convertAngle(60);
+direction.asteroidType = "basic";
+direction.asteroidNb = 5;
+direction.asteroidLife = 1;
+direction.moneyEarned = 5;
 wave.AddDirection(direction);
 
 direction = LevelDescriptor.CreateDirection();
-direction.angle = 45.0 * 3.14 / 180.0;
-direction.asteroidLife = 8;
-direction.asteroidNb = 1;
+direction.angle = convertAngle(100);
 direction.asteroidType = "basic";
-direction.accuracy = 0.1;
+direction.asteroidNb = 5;
+direction.asteroidLife = 1;
+direction.moneyEarned = 5;
+
+wave.AddDirection(direction);
+round.AddWave(wave);
+LevelDescriptor.AddRound(round);
+
+// fourth round
+round = LevelDescriptor.CreateRound();
+round.name = "Add a new planet";
+wave = LevelDescriptor.CreateWave();
+
+direction = LevelDescriptor.CreateDirection();
+direction.angle = convertAngle(-45);
+direction.asteroidType = "basic";
+direction.asteroidNb = 5;
+direction.asteroidLife = 1;
+direction.moneyEarned = 5;
+wave.AddDirection(direction);
+
+direction = LevelDescriptor.CreateDirection();
+direction.angle = convertAngle(90);
+direction.asteroidType = "basic";
+direction.asteroidNb = 5;
+direction.asteroidLife = 1;
+direction.moneyEarned = 5;
 wave.AddDirection(direction);
 
 round.AddWave(wave);
-
 LevelDescriptor.AddRound(round);
 
+// fifth round
+round = LevelDescriptor.CreateRound();
+round.name = "New asteroid type";
+wave = LevelDescriptor.CreateWave();
+
+direction = LevelDescriptor.CreateDirection();
+direction.angle = convertAngle(-100);
+direction.asteroidType = "splittable";
+direction.asteroidNb = 5;
+direction.asteroidLife = 2;
+direction.asteroidScale = 2;
+direction.moneyEarned = 5;
+wave.AddDirection(direction);
+
+direction = LevelDescriptor.CreateDirection();
+direction.angle = convertAngle(50);
+direction.asteroidType = "splittable";
+direction.asteroidNb = 5;
+direction.asteroidLife = 2;
+direction.asteroidScale = 2;
+direction.moneyEarned = 5;
+wave.AddDirection(direction);
+
+round.AddWave(wave);
+LevelDescriptor.AddRound(round);
+
+// sixth round
+round = LevelDescriptor.CreateRound();
+round.name = "Add lazer on a planet";
+wave = LevelDescriptor.CreateWave();
+
+direction = LevelDescriptor.CreateDirection();
+direction.angle = convertAngle(0);
+direction.asteroidType = "splittable";
+direction.asteroidNb = 10;
+direction.asteroidLife = 4;
+direction.asteroidScale = 2;
+direction.moneyEarned = 5;
+direction.accuracy = 0;
+wave.AddDirection(direction);
+
+direction = LevelDescriptor.CreateDirection();
+direction.angle = convertAngle(30);
+direction.asteroidType = "splittable";
+direction.asteroidNb = 10;
+direction.asteroidLife = 4;
+direction.asteroidScale = 2;
+direction.moneyEarned = 5;
+direction.accuracy = 0;
+wave.AddDirection(direction);
+
+direction = LevelDescriptor.CreateDirection();
+direction.angle = convertAngle(-60);
+direction.asteroidType = "splittable";
+direction.asteroidNb = 10;
+direction.asteroidLife = 4;
+direction.asteroidScale = 2;
+direction.moneyEarned = 5;
+direction.accuracy = 0;
+wave.AddDirection(direction);
+
+round.AddWave(wave);
+LevelDescriptor.AddRound(round);
+
+// seventh round
+round = LevelDescriptor.CreateRound();
+round.name = "Add a bouncy shield";
+round.moneyEarned = 50;
+wave = LevelDescriptor.CreateWave();
 
 
+direction = LevelDescriptor.CreateDirection();
+direction.angle = convertAngle(-30);
+direction.asteroidType = "basic";
+direction.asteroidNb = 5;
+direction.asteroidLife = 2;
+direction.asteroidScale = 2;
+direction.moneyEarned = 5;
+direction.accuracy = 0;
+wave.AddDirection(direction);
 
+direction = LevelDescriptor.CreateDirection();
+direction.angle = convertAngle(-35);
+direction.asteroidType = "basic";
+direction.asteroidNb = 5;
+direction.asteroidLife = 2;
+direction.asteroidScale = 2;
+direction.moneyEarned = 5;
+direction.accuracy = 0;
+wave.AddDirection(direction);
+
+direction = LevelDescriptor.CreateDirection();
+direction.angle = convertAngle(-25);
+direction.asteroidType = "basic";
+direction.asteroidNb = 5;
+direction.asteroidLife = 2;
+direction.asteroidScale = 2;
+direction.moneyEarned = 5;
+direction.accuracy = 0;
+wave.AddDirection(direction);
+
+round.AddWave(wave);
+LevelDescriptor.AddRound(round);
+
+// eight round
+round = LevelDescriptor.CreateRound();
+round.name = "add a new planet with destruction shield";
+round.moneyEarned = 50;
+wave = LevelDescriptor.CreateWave();
+
+direction = LevelDescriptor.CreateDirection();
+direction.angle = convertAngle(0);
+direction.asteroidType = "basic";
+direction.asteroidNb = 10;
+direction.asteroidLife = 2;
+direction.moneyEarned = 5;
+wave.AddDirection(direction);
+
+direction = LevelDescriptor.CreateDirection();
+direction.angle = convertAngle(135);
+direction.asteroidType = "splittable";
+direction.asteroidNb = 5;
+direction.asteroidLife = 2;
+direction.moneyEarned = 5;
+wave.AddDirection(direction);
+
+direction = LevelDescriptor.CreateDirection();
+direction.angle = convertAngle(270);
+direction.asteroidType = "ship";
+direction.asteroidNb = 1;
+direction.asteroidLife = 2;
+direction.moneyEarned = 5;
+wave.AddDirection(direction);
+
+round.AddWave(wave);
+LevelDescriptor.AddRound(round);
 
 // level loading
 LevelDescriptor.Load();
+
+function convertAngle(angle : float)
+{
+	return (angle * 3.14 / 180.0);
+}
+
